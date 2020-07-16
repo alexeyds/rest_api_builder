574260cd065639b8715ad951f9068f897c5f9e87
alexeyds <org.alwo@gmail.com>
2020-07-16 15:38:26 +0300

    Cleanup WebMockRequestExpectations

--- lib/rest_api_builder/webmock_request_expectations.rb
+++ lib/rest_api_builder/webmock_request_expectations.rb
@@ -6,39 +6,8 @@ module RestAPIBuilder
     include WebMock::API
     include RestAPIBuilder::UrlHelper
 
-    def expect_json_execute(response_body: nil, **options)
-      response_body &&= JSON.generate(response_body)
-      expect_execute(**options, response_body: response_body)
-    end
-
-    def expect_execute(
-      base_url:,
-      method:,
-      status: 200,
-      response_body: nil,
-      response_headers: nil,
-      request_body: nil,
-      request_headers: nil,
-      path: nil,
-      query: nil,
-      should_timeout: false
-    )
-      request = stub_request(method, full_url(base_url, path))
-
-      add_request_expectations(request, { body: request_body, query: query, headers: request_headers })
-
-      if should_timeout
-        request.to_timeout
-      else
-        request.to_return(status: status, body: response_body, headers: response_headers)
-      end
-    end
-
-    private
-
-    def add_request_expectations(request, expectations)
-      expectations = expectations.compact
-      request.with(expectations) unless expectations.empty?
+    def expect_execute(base_url:, method:, path: nil)
+      stub_request(method, full_url(base_url, path))
     end
   end
 
--- test/rest_api_builder/webmock_request_expectations_test.rb
+++ test/rest_api_builder/webmock_request_expectations_test.rb
@@ -12,88 +12,11 @@ describe RestAPIBuilder::WebMockRequestExpectations do
       assert_equal('', result[:body])
     end
 
-    it 'has :status parameter' do
-      expectations.expect_execute(**get_test, status: 404)
-      result = request.execute(**get_test)
-
-      assert_equal(404, result[:status])
-    end
-
-    it 'has :response_body parameter' do
-      expectations.expect_execute(**get_test, response_body: 'hi')
-      result = request.execute(**get_test)
-
-      assert_equal('hi', result[:body])
-    end
-
-    it 'has response_headers parameter' do
-      expectations.expect_execute(**get_test, response_headers: { 'X-Test' => true })
-      result = request.execute(**get_test)
-
-      assert_equal("true", result[:headers][:x_test])
-    end
-
-    it 'matches :request_body' do
-      expectations.expect_execute(method: :post, base_url: 'test.com', request_body: 'hello')
-
-      assert_raises WebMock::NetConnectNotAllowedError do
-        request.execute(method: :post, base_url: 'test.com')
-      end
-    end
-
-    it 'doesnt register body expectation if no :request_body is provided' do
-      expectations.expect_execute(method: :post, base_url: 'test.com')
-      result = request.execute(method: :post, base_url: 'test.com', body: 'hi')
-
-      assert_equal(true, result[:success])
-    end
-
     it 'has :path parameter' do
       expectations.expect_execute(**get_test, path: '/orders')
       result = request.execute(**get_test, path: '/orders')
 
       assert_equal(true, result[:success])
-    end
-
-    it 'has :query parameter' do
-      expectations.expect_execute(**get_test, path: '/orders', query: { test: 1 })
-      result = request.execute(**get_test, path: '/orders', query: { test: 1 })
-
-      assert_equal(true, result[:success])
-    end
-
-    it 'matches :request_headers' do
-      expectations.expect_execute(**get_test, request_headers: { 'X-Test' => true })
-
-      assert_raises WebMock::NetConnectNotAllowedError do
-        request.execute(**get_test)
-      end
-    end
-
-    it 'has :should_timeout parameter' do
-      expectations.expect_execute(**get_test, should_timeout: true)
-
-      assert_raises RestClient::Exceptions::OpenTimeout do
-        request.execute(**get_test)
-      end
-    end
-  end
-
-  describe '#expect_json_execute' do
-    it 'behaves like expect_execute' do
-      expectations.expect_json_execute(**get_test)
-      result = request.json_execute(**get_test)
-
-      assert_equal(true, result[:success])
-      assert_equal(200, result[:status])
-      assert_equal('', result[:body])
-    end
-
-    it 'converts response_body to json' do
-      expectations.expect_json_execute(**get_test, response_body: { a: 1 })
-      result = request.json_execute(**get_test)
-
-      assert_equal({ 'a' => 1 }, result[:body])
     end
   end
 
